05be824be1c083dfec09bbbf6cf1ac189562e328
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-15 02:27:49 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguements)
     array = []
     self.each do |place, animal|
-      binding.pry
+      # binding.pry
       if arguements.include?(place)
         array << animal
       end
