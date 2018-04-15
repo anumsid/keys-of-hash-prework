126a1510e04177fd3a627ea639001464459155cd
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-15 02:04:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
   def keys_of(arguments)
     array = []
+    hash.each do |place, animal|
+      if arguments.include?(place)
+        array << animal
+      end
+    end
   end
+  array
 end
